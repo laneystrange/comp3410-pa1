3ef8e24fdd1fbd00da71296476df7aa8e6ef29db
Pronis <redstrom83@gmail.com>
2015-02-10 02:14:54 -0600

    Add fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,13 +1,10 @@
 # COMP3410 Program Template
-# Author: Your Name
-# Assignment: PA[X]
-# Date: Date of submission
-
-# Turn in one .asm file per assignment component
-# Remember to submit it as a pull request to the GitHub repo for the assignment
+# Author: Robert L. Edstrom
+# Assignment: PA[1]
+# Date: 
 
 # fibonacci.asm
-# Input: None
+# Input: size - which indicates the length of the array
 # Output: Print to terminal N Fibonacci numbers
 
 ##########################################################
@@ -16,47 +13,63 @@
 
 	.data
 
-fibs:	.word   0 : 9         # create an array variable named "fibs" of 9 word-length elements (4 bytes each)
-size:	.word  9              # create a single integer variable named "size" that indicates the length of the array
+fibs:	.word   0 : 9	# create an array variable named "fibs" of 9 word-length elements (4 bytes each)
+size:	.asciiz "Enter the value for the Fibonacci Sequence: "	# create a single integer variable named "size" that indicates the length of the array
 
 ###############################################
 # .text segment. Assembly instructions go here.
 ###############################################
 	.text
-	      la   $s0, fibs        # load address of array into $s0
-	      la   $s5, size        # load address of size variable into $s5
-	      lw   $s5, 0($s5)      # load array size from its address in the register
+	      
+input:
+		# Changed code
+		 la $a0, size		# Load address of size message into $a0
+	     	 li $v0, 4		# Setup for printing size message 
+	     	 syscall		# Syscall prints size message
+	     	 
+	     	 li $v0, 5		# Setup for accepting an integer from the user
+	     	 syscall		# Syscall to prompt the user for an integer
+	     	
+	     	 blt $v0, 1, input	# Check if input is less than 1. If so start over
+	     	 bgt $v0, 9, input	# Check if input is greater than 9. If so start over
+	     	
+	     	 move $s5, $v0		# Move the integer stored in $v0 to $s5
+	       # End changed code
+	   
+		 la   $s0, fibs         # load address of array into $s0
 
-	      li   $s2, 1           # Initialize the Fibonacci numbers with value 1, stored in $s2
-	      sw   $s2, 0($s0)      # Set fibs[0] to 1
-	      sw   $s2, 4($s0)      # Set fibs[1] to 1
-	      addi $s1, $s5, -2     # Counter for loop, will execute (size-2) times
+	      	 li   $s2, 1           # Initialize the Fibonacci numbers with value 1, stored in $s2
+	     	 sw   $s2, 0($s0)      # Set fibs[0] to 1
+	     	 sw   $s2, 4($s0)      # Set fibs[1] to 1
+	     	 addi $s1, $s5, -2     # Counter for loop, will execute (size-2) times
 
-	      # Loop to compute each Fibonacci number using the previous two Fib. numbers.
-	      # On line 37, "loop" is a label
-loop:	 lw   $s3, 0($s0)      # Get value from array fibs[i-2]
-	      lw   $s4, 4($s0)      # Get value from array fibs[i-1]
-	      add  $s2, $s3, $s4    # fibs[i] = fibs[i-1] + fibs[i-2]
-	      sw   $s2, 8($s0)      # Store newly computed fibs[i] in array
-	      addi $s0, $s0, 4      # increment address for next fibonacci entry
-	      addi $s1, $s1, -1     # decrement loop counter
-	      bgtz $s1, loop        # repeat while not finished
+	     	 # Loop to compute each Fibonacci number using the previous two Fib. numbers.
+	    	 # On line 37, "loop" is a label
+loop:	 	 lw   $s3, 0($s0)      # Get value from array fibs[i-2]
+	     	 lw   $s4, 4($s0)      # Get value from array fibs[i-1]
+	     	 add  $s2, $s3, $s4    # fibs[i] = fibs[i-1] + fibs[i-2]
+	      	 sw   $s2, 8($s0)      # Store newly computed fibs[i] in array
+	     	 addi $s0, $s0, 4      # increment address for next fibonacci entry
+	     	 addi $s1, $s1, -1     # decrement loop counter
+	     	 bgtz $s1, loop        # repeat while not finished
 
-	      # Now the Fibonacci numbers are computed and stored in array. Print them.
-	      la   $a0, fibs        # first argument for print (array)
-	      add  $a1, $zero, $s5  # second argument for print (size)
-	      jal  print            # call print routine (note the 'print' label on line 68)
+	         # Now the Fibonacci numbers are computed and stored in array. Print them.
+	         la   $a0, fibs        # first argument for print (array)
+	         add  $a1, $zero, $s5  # second argument for print (size)
+	         jal  print            # call print routine (note the 'print' label on line 68)
 
-	      # The program is finished. Exit.
-	      li   $v0, 10          # system call for exit
-	      syscall               
+                 # The program is finished. Exit.
+	         li   $v0, 10          # system call for exit
+	         syscall               
+	         
+		
 
 	###############################################################
 	# Subroutine to print the numbers on one line. Another .data segment.
 	###########################################################
 	      .data
 	      
-space:	.asciiz  " "          				# Print a space between each pair of numbers
+space:	 .asciiz  " "          				# Print a space between each pair of numbers
 head:	 .asciiz  "The Fibonacci numbers are:\n" 	# Print a little helpful intro
 
 ###########################################################
@@ -65,13 +78,13 @@ head:	 .asciiz  "The Fibonacci numbers are:\n" 	# Print a little helpful intro
 
 	      .text
 	      
-print:	add  $t0, $zero, $a0  # starting address of array of data to be printed
+print:	      add  $t0, $zero, $a0  # starting address of array of data to be printed
 	      add  $t1, $zero, $a1  # initialize loop counter to array size
 	      la   $a0, head        # load address of the print heading string
 	      li   $v0, 4           # specify that you're printing a string
 	      syscall               # print the heading string
 
-out:	  lw   $a0, 0($t0)      # load the integer to be printed (the current Fib. number)
+out:	      lw   $a0, 0($t0)      # load the integer to be printed (the current Fib. number)
 	      li   $v0, 1           # specify that you're printing an integer
 	      syscall               # print fibonacci number
 
@@ -85,4 +98,4 @@ out:	  lw   $a0, 0($t0)      # load the integer to be printed (the current Fib. 
 
 	      jr   $ra              # return from subroutine
 	# End of subroutine to print the numbers on one line
-	###############################################################
+	##############################################################
