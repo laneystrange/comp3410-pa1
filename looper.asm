f932d3d86f5056c7b45a85d390c3b17edd8c6f03
Aaron Marshall <atmars1@hotmail.com>
2015-02-10 20:46:56 -0600

    Test

---
+++ looper.asm
@@ -0,0 +1,20 @@
+# COMP3410 Program Template
+# Author: Aaron Marshall
+# Assignment: PA[1]
+# Date: 2/10/14
+
+	.text
+  
+  li $s0, 1 #1
+  addi $s1, $s0, 2 #3
+loop:
+	add $s0, $s1, $s0 # s0 + s1
+	addi $s1, $s1, 2 #s1+2
+	ble $s1, 15, loop # starts loop if <= 15
+	
+  add $a0,$zero, $s0 #prints final answer
+  li $v0, 1
+  syscall
+  
+  li $v0, 10 #exits
+  syscall
