d4a3930f7f0c0cb24fe1e2051c054a0d0198fad8
Daniel Thompson <dthmps10@memphis.edu>
2015-02-13 00:14:36 -0600

    <message>

--- part2.asm
+++ part2.asm
@@ -11,6 +11,7 @@
 name: .asciiz "by Daniel Thompson"
 title: .asciiz "Welcome to Programming Assignment 1: Part 2."
 desc: .asciiz "This program takes two numbers from user input and performs 3 calculations on them."
+bye: .asciiz "The end."
 
 prompt: .asciiz "Enter two numbers:"
 num1: .word 0 # variable to hold the first number
@@ -29,21 +30,21 @@ space: .asciiz "\n" # used to space lines
 
 
 ##### print title, description, and name #####
-li $v0, 4 # call to print to terminal
+li $v0, 4 # call to print string to terminal
 la $a0, title # load address to print title
 syscall
 
 la $a0, space # space between lines
 syscall
 
-li $v0, 4 # call to print to terminal
+li $v0, 4 # call to print string to terminal
 la $a0, desc # load address to print intro
 syscall
 
 la $a0, space # space between lines
 syscall
 
-li $v0, 4 # call to print to terminal
+li $v0, 4 # call to print string to terminal
 la $a0, name # load address to print name
 syscall
 
@@ -72,18 +73,41 @@ sw $v0, num2 # store the second integer in memory
 la $t1, num2 # load the second integer into $t1 register
 
 
-##### perform the calculations #####
+##### perform the calculations and print to terminal #####
 add $s0, $t0, $t1 # add the two integers and store in $s0
 sw $s0, sum # store the result in sum
+
+li $v0, 1 # call to print int to terminal
+la $a0, ($s0)
+syscall
+
+la $a0, space # space between lines
+syscall
 
 sub $s1, $t0, $t1 # subtract the second integer from the first and store in $s1
 sw $s1, difference # store the result in difference
 
-mult $s2, $t0, $t1 # multiply the two integers
+li $v0, 1 # call to print int to terminal
+la $a0, ($s1)
+syscall
+
+la $a0, space # space between lines
+syscall
+
+mul $s2, $t0, $t1 # multiply the two integers
 sw $s2, product # store the result in product
 
+li $v0, 1 # call to print int to terminal
+la $a0, ($s2)
+syscall
 
+la $a0, space # space between lines
+syscall
 
+##### print concluding message #####
+li $v0, 4 # call to print string to terminal
+la $a0, bye
+syscall
 
 ##### exit the program #####
 li $v0, 10 # exit
