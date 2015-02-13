cc4cb9173f73737f73f8df2604d67d5556426d61
Daniel Thompson <dthmps10@memphis.edu>
2015-02-12 22:18:42 -0600

    <message>

---
+++ part2.asm
@@ -0,0 +1,17 @@
+# COMP3410
+# Author: Daniel Thompson
+# Assignment: PA[1] Part 2
+# Date: 2/12/15
+
+#######################################
+# .data segment
+#######################################
+
+.data
+name: .asciiz "Daniel Thompson"
+title: .asciiz "Programming Assignment: Part 2"
+desc: .asciiz "This program takes two numbers from user input and performs 3 calculations on them."
+
+######################################
+# .text segment
+######################################
