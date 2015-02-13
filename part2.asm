cff5e4c2da670801ffe10752bee2b0abbc4056c8
Daniel Thompson <dthmps10@memphis.edu>
2015-02-12 22:38:49 -0600

    <message>

--- part2.asm
+++ part2.asm
@@ -6,8 +6,8 @@
 #######################################
 # .data segment
 #######################################
-
 .data
+
 name: .asciiz "Daniel Thompson"
 title: .asciiz "Welcome to Programming Assignment 1: Part 2"
 desc: .asciiz "This program takes two numbers from user input and performs 3 calculations on them."
@@ -15,9 +15,19 @@ desc: .asciiz "This program takes two numbers from user input and performs 3 cal
 ######################################
 # .text segment
 ######################################
+.text
 
 li $v0, 4 # call to print to terminal
 la $a0, title # load address to print title
-la $a1, desc # load address to print intro
-la $a2, name # load address to print name
+syscall
+
+li $v0, 4 # call to print to terminal
+la $a0, desc # load address to print intro
+syscall
+
+li $v0, 4 # call to print to terminal
+la $a0, name # load address to print name
+syscall
+
+li $v0, 10 # exit
 syscall
